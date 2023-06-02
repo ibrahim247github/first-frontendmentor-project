fb852a949f92691d3f5d1381ac10ef3055181f89
Ibrahim247github <ibrahimsaheed333@gmail.com>
2023-06-02 08:24:24 +0100

    Modify javascript to enable users to be able to click on the link text to open the dropdown menu

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let featureDropdownMenu = document.getElementById("featureDropdownMenu");
 let companyDropdownMenu = document.getElementById("companyDropdownMenu");
+let toggleFeatures = document.getElementById("toggleFeatures");
+let toggleCompany = document.getElementById("toggleCompany");
 
 let companyUpIcon = document.getElementById("company-up");
 let companyDownIcon = document.getElementById("company");
@@ -29,4 +31,28 @@ function hideCompany() {
     companyDropdownMenu.style.display = "none";
     companyDownIcon.style.display = "inline-block";
     companyUpIcon.style.display = "none";
+}
+
+function showF(showFeatures,hideFeatures) {
+    if(toggleFeatures.classList.contains("showFeatures")) {
+        showFeatures();
+        toggleFeatures.classList.toggle("showFeatures"); 
+        toggleFeatures.classList.toggle("hideFeatures");               
+    }else if(toggleFeatures.classList.contains("hideFeatures")) {
+        hideFeatures();
+        toggleFeatures.classList.toggle("hideFeatures");               
+        toggleFeatures.classList.toggle("showFeatures"); 
+    }
+}
+
+function showC(showCompany,hideCompany) {
+    if(toggleCompany.classList.contains("showCompany")) {
+        showCompany();
+        toggleCompany.classList.toggle("showCompany"); 
+        toggleCompany.classList.toggle("hideCompany");               
+    }else if(toggleCompany.classList.contains("hideCompany")) {
+        hideCompany();
+        toggleCompany.classList.toggle("hideCompany");               
+        toggleCompany.classList.toggle("showCompany"); 
+    }
 }
